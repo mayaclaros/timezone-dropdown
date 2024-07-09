28e155022713b18cdc168c218afdb6c9db4cab0b
mayaclaros <123900092+mayaclaros@users.noreply.github.com>
2024-07-08 23:00:08 -0400

    updates to html and js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 function showSelectedCountry(event) {
   if (event.target.value === "Sydney") {
-    alert();
+    let sydneyTime = moment()
+      .tz("Australia/Sydney")
+      .format("[It is] dddd, MMMM d, YYYY [in Australia / Sydney]");
+
+    let sydneyElement = document.querySelector("#Sydney");
+    sydneyElement.innerHTML = sydneyTime;
+
+    alert(sydneyTime);
   }
+
   if (event.target.value === "Paris") {
-    alert();
+    let parisTime = moment()
+      .tz("Europe/Paris")
+      .format("[It is] dddd, MMMM d, YYYY [in Europe/Paris]");
+
+    let parisElement = document.querySelector("#Paris");
+    parisElement.innerHTML = parisTime;
+    alert(parisTime);
   }
   if (event.target.value === "Tokyo") {
-    alert();
+    let tokyoTime = moment()
+      .tz("Asia/Tokyo")
+      .format("[It is] dddd, MMMM d, YYYY [in Asia/Tokyo]");
+
+    let tokyoElement = document.querySelector("#Tokyo");
+    tokyoElement.innerHTML = tokyoTime;
+    alert(tokyoTime);
   }
 }
+
 let countrySelect = document.querySelector("#country");
 countrySelect.addEventListener("change", showSelectedCountry);
